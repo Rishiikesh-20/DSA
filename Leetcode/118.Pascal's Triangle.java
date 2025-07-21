c4a4b9bbbee9c5fcf162e9c9e77b84acc652da6a
Rishiikesh <rishiikeshsk@gmail.com>
2025-07-22 00:50:26 +0530

    118.Pascal's Triangle.java (striver sol)

--- Leetcode/118.Pascal's Triangle.java
+++ Leetcode/118.Pascal's Triangle.java
@@ -5,12 +5,11 @@ class Solution {
         List<List<Integer>> arr= new ArrayList<>();
         for(int i=0;i<numRows;i++){
             arr.add(new ArrayList<>());
-            for(int j=0;j<=i;j++){
-                if(j==0 || j==(i)){
-                    arr.get(i).add(1);
-                }else{
-                    arr.get(i).add(arr.get(i-1).get(j-1)+arr.get(i-1).get(j));
-                }
+            int ans=1;
+             arr.get(i).add(ans);
+            for(int j=1;j<=i;j++){
+                ans=ans*(i-j+1)/(j);
+                arr.get(i).add(ans);
             }
         }
         return arr;
