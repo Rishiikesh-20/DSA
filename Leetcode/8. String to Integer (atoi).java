38db4a351ee1ce93f01c8265521de58d52a14902
Rishiikesh <rishiikeshsk@gmail.com>
2025-08-17 00:10:09 +0530

    done 8. String to Integer (atoi) (Recursion sol)

--- Leetcode/8. String to Integer (atoi).java
+++ Leetcode/8. String to Integer (atoi).java
@@ -1,6 +1,77 @@
 package Leetcode;
 
-class Solution {
+
+class Solution1 {
+    public int myAtoi(String s) {
+        int[] sign=new int[1];
+        sign[0]=-1;
+        String result=recur(s,0,0,0,sign);
+        if(result.length()==0){
+            return 0;
+        }
+        if(sign[0]==0){
+            return Integer.parseInt(result);
+        }else{
+            if(Long.parseLong(result)+Integer.MIN_VALUE==0){
+                return Integer.MIN_VALUE;
+            }
+            return -1*Integer.parseInt(result);
+        }
+    }
+
+    public static String recur(String s,int i,int flag,int count,int[] sign){
+        if(i>=s.length() || (flag==1 && (s.charAt(i)=='-' || s.charAt(i)=='+'))){
+            return "";
+        }
+
+        if(s.charAt(i)==' ' && sign[0]==-1){
+            return recur(s,i+1,flag,count,sign);
+        }
+
+        if(flag==0 && sign[0]==-1 && (s.charAt(i)=='-' || s.charAt(i)=='+')){
+            sign[0]=s.charAt(i)=='-'?1:0;
+            return recur(s,i+1,flag,count,sign);
+        }
+        if(!Character.isDigit(s.charAt(i))){
+            return "";
+        }else if(s.charAt(i)=='0' && flag==0){
+            if(sign[0]==-1){
+                sign[0]=0;
+            }
+            return recur(s,i+1,flag,count,sign);
+        }
+        flag=1;
+        count++;
+        if(sign[0]==-1){
+            sign[0]=0;
+        }
+        if(count>10){
+            return sign[0]==1?""+Integer.MIN_VALUE:""+Integer.MAX_VALUE;
+        }
+        String result=recur(s,i+1,flag,count,sign);
+        if(result.equals(""+Integer.MIN_VALUE) || result.equals( ""+Integer.MAX_VALUE)){
+            return result;
+        }
+        Long num=Long.parseLong(s.charAt(i)+result);
+        if(sign[0]==1){
+            if(Integer.MIN_VALUE+num>0){
+                return ""+Integer.MIN_VALUE;
+            }else{
+                return s.charAt(i)+result;
+            }
+        }else  if(sign[0]==0){
+             if(num-Integer.MAX_VALUE>0){
+                return ""+Integer.MAX_VALUE;
+            }else{
+                return s.charAt(i)+result;
+            }
+        }
+        return "";
+    }
+}
+
+// Iterative Solution
+class Solution2 {
     public int myAtoi(String s) {
         int n=s.length();
         int sign=0;
