b3eb71d8affb9ccf6e9411ca217ff733236868f3
Rishiikesh <154777301+Rishiikesh-20@users.noreply.github.com>
2026-06-14 18:14:28 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (55.91%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,23 +1,30 @@
 class Solution {
     public int trap(int[] arr) {
         int n=arr.length;
-        int[] left=new int[n];
-        int[] right=new int[n];
-        left[0]=arr[0];
-        for(int i=1;i<n;i++){
-            left[i]=Math.max(left[i-1],arr[i]);
-        }
-        right[n-1]=arr[n-1];
-        for(int i=n-2;i>=0;i--){
-            right[i]=Math.max(right[i+1],arr[i]);
-        }
+
+        int left=0;
+        int right=n-1;
+
+        int leftMax=arr[0];
+        int rightMax=arr[n-1];
 
         int total=0;
-
-        for(int i=0;i<n;i++){
-            int val=Math.min(left[i],right[i])-arr[i];
-            if(val>=0)
-                total+=val;
+        while(left<=right){
+            if(arr[left]<arr[right]){
+                if(arr[left]>=leftMax){
+                    leftMax=arr[left];
+                }else{
+                    total+=(leftMax-arr[left]);
+                }
+                left++;
+            }else{
+                if(arr[right]>=rightMax){
+                    rightMax=arr[right];
+                }else{
+                    total+=(rightMax-arr[right]);
+                }
+                right--;
+            }
         }
         return total;
     }
