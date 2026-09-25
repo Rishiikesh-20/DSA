09a855a3d473a8485373438b335bd63ec84bb20b
Rishiikesh <154777301+Rishiikesh-20@users.noreply.github.com>
2026-09-25 13:04:15 +0530

    Time: 9 ms (33.87%) | Memory: 49.2 MB (42.72%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,30 +1,24 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->{
-            return Integer.compare(a[0],b[0]);
-        });
-        List<List<Integer>> list=new ArrayList<>();
-        int n=intervals.length;
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
         int start=intervals[0][0];
         int end=intervals[0][1];
+        int n=intervals.length;
+        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
         for(int i=1;i<n;i++){
-            if(intervals[i][0]<=end){
-                if(intervals[i][1]>end){
-                    end=intervals[i][1];
-                }
-            }else{
-                list.add(List.of(start,end));
+            if(end>=intervals[i][0] && end<intervals[i][1]){
+                end=intervals[i][1];
+            }else if(end<intervals[i][1]){
+                list.add(new ArrayList<>(List.of(start,end)));
                 start=intervals[i][0];
                 end=intervals[i][1];
             }
         }
-        list.add(List.of(start,end));
+        list.add(new ArrayList<>(List.of(start,end)));
 
-        int len=list.size();
+        int[][] result=new int[list.size()][2];
 
-        int[][] result=new int[len][2];
-
-        for(int i=0;i<len;i++){
+        for(int i=0;i<list.size();i++){
             result[i][0]=list.get(i).get(0);
             result[i][1]=list.get(i).get(1);
         }
